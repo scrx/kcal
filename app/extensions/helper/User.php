a8e874b0f280e7d4d9a223b4df1f0996a3745ce6
scrx <mattpiskorz@gmail.com>
2013-01-24 00:43:01 +0100

    Missing FoodController, sry zoltax san, added username in the top bar

--- app/extensions/helper/User.php
+++ app/extensions/helper/User.php
@@ -9,6 +9,15 @@ class User extends \lithium\template\helper {
 		return Auth::check('member', $this->_context->_config['request']);
 	}
 	
+	
+  // public function fullName() {
+
+  //      $user = self::user();
+
+   //     return $user["first_name"] . " " . $user["last_name"];
+    //}
+	//
+		
 	//public function checkPassword() {
 	//	return Password::check()
 	//}
